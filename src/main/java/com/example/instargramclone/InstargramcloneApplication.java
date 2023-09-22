de0b91fffbc9644a92a01eac2d591f25154bc67c
MINKYU <mingyu1285@naver.com>
2023-09-22 21:45:46 +0900

    Initial commit

---
+++ src/main/java/com/example/instargramclone/InstargramcloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.instargramclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstargramcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstargramcloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/instargramclone/InstargramcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instargramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstargramcloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
